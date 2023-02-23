cb46368963873636c8e177d7b32812eb3215527e
Jade Fox <guiltier.gear@gmail.com>
2023-02-22 17:19:43 -0800

    Removed encoding module from lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -17,7 +17,7 @@ lualine.setup {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff' },
         lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_x = { 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
     }
