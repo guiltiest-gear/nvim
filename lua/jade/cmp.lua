43725ba5cd18a02494a293b0b16d694107935da6
Jade Fox <guiltier.gear@gmail.com>
2023-03-01 15:29:27 -0800

    Disable nvim-cmp while writing comments

--- lua/jade/cmp.lua
+++ lua/jade/cmp.lua
@@ -123,5 +123,16 @@ cmp.setup {
     },
     experimental = {
         ghost_text = true
-    }
+    },
+    enabled = function()
+        -- Disable completion in comments
+        local context = require 'cmp.config.context'
+        -- Keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == 'c' then
+            return true
+        else
+            return not context.in_treesitter_capture("comment")
+            and not context.in_syntax_group("Comment")
+        end
+    end
 }
