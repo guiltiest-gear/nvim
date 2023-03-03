bc5df02535f8fffe2453e8f3da795b60346e071c
Jade Fox <guiltier.gear@gmail.com>
2023-03-02 16:48:08 -0800

    Added gitsigns integration for null-ls

--- lua/jade/lsp/null-ls.lua
+++ lua/jade/lsp/null-ls.lua
@@ -7,6 +7,8 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup {
     debug = false,
@@ -14,6 +16,7 @@ null_ls.setup {
         formatting.stylua,
         formatting.markdownlint,
         diagnostics.markdownlint,
-        diagnostics.luacheck
+        diagnostics.luacheck,
+        code_actions.gitsigns
     }
 }
