e4b6631ff6fdb268db76c4d319342a6debdf7248
Jade Fox <guiltier.gear@gmail.com>
2023-04-05 21:06:22 -0700

    Added nvim-navic config to lsp settings

--- lua/jade/lsp/settings/clangd.lua
+++ lua/jade/lsp/settings/clangd.lua
@@ -1,1 +1,7 @@
-require 'lspconfig'.clangd.setup {}
+local navic = require('nvim-navic')
+
+require 'lspconfig'.clangd.setup {
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end
+}
