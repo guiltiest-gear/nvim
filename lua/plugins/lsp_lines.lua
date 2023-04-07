4aaf770d981d92701ab5009d1874efb17fc8a874
Jade Fox <guiltier.gear@gmail.com>
2023-04-07 08:49:04 -0700

    Moved vim.diagnostic.config to plugins/lsp_lines.lua

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -1,9 +1,12 @@
 return {
-	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-	event = "LspAttach",
-	config = function()
-		local lsp_lines = require("lsp_lines")
-		lsp_lines.setup()
-		vim.keymap.set("", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-	end,
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    event = "LspAttach",
+    config = function()
+        local lsp_lines = require("lsp_lines")
+        lsp_lines.setup()
+        vim.keymap.set("", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+        vim.diagnostic.config({
+            virtual_text = false
+        })
+    end,
 }
