bb10f8b56b1bd884d03f92a18ac258e9bb8c2488
Jade Fox <guiltier.gear@gmail.com>
2023-04-08 18:11:05 -0700

    Lazy loaded indent-blankline.nvim on BufAdd

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,6 +1,6 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    event = "VeryLazy",
+    event = "BufAdd",
     config = function()
         local blankline = require("indent_blankline")
         blankline.setup({
