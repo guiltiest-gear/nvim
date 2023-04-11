d35526bda75d37b8f53627910390920b357f85f1
Jade Fox <guiltier.gear@gmail.com>
2023-04-10 17:28:40 -0700

    Changed bufferline.nvim to load on BufEnter

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,6 +2,6 @@ return {
     "akinsho/bufferline.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
     version = "v3.*",
-    event = "BufAdd",
+    event = "BufEnter",
     config = true
 }
