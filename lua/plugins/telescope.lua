d5cb05e24b2256a9db0de7c7ad8cc43b190e9237
Jade Fox <guiltier.gear@gmail.com>
2023-04-11 22:38:50 -0700

    Moved telescope-fzf-native.nvim as a dependency for telescope.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make"
+        }
+    },
     branch = "0.1.x",
     cmd = "Telescope",
     config = function()
