eb686343d31bf0672a0cfc30f45596f8f2c60abb
Jade Fox <guiltier.gear@gmail.com>
2023-04-11 21:12:22 -0700

    Changed build option for nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    build = function()
+        require('nvim-treesitter.install').update({ with_sync = true })
+    end,
     event = {
         "BufReadPre",
         "BufNewFile"
