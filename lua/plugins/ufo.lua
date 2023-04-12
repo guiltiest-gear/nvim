cfc2487b6c84ef31c0ef5c99e21383265ea082c2
Jade Fox <guiltier.gear@gmail.com>
2023-04-11 18:09:10 -0700

    Changed nvim-ufo lazy load events

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,7 +1,10 @@
 return {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
-    event = "VeryLazy",
+    event = {
+        "BufReadPre",
+        "BufNewFile"
+    },
     config = function()
         local ufo = require('ufo')
         ufo.setup({
