bab419a70860c56712fba2702387bf75e87c0052
Jade Fox <guiltier.gear@gmail.com>
2023-04-12 19:21:21 -0700

    Minor formatting changes to gitsigns.nvim config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -20,28 +20,33 @@ return {
         local gitsigns = require('gitsigns')
         gitsigns.setup({
             signs = {
-                add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+                add = {
+                    hl = 'GitSignsAdd',
+                    text = '+',
+                    numhl = 'GitSignsAddNr',
+                    linehl = 'GitSignsAddLn'
+                },
                 change = {
                     hl = 'GitSignsChange',
-                    text = '▎',
+                    text = '~',
                     numhl = 'GitSignsChangeNr',
                     linehl = 'GitSignsChangeLn',
                 },
                 delete = {
                     hl = 'GitSignsDelete',
-                    text = '契',
+                    text = '-',
                     numhl = 'GitSignsDeleteNr',
                     linehl = 'GitSignsDeleteLn',
                 },
                 topdelete = {
                     hl = 'GitSignsDelete',
-                    text = '契',
+                    text = '-',
                     numhl = 'GitSignsDeleteNr',
                     linehl = 'GitSignsDeleteLn',
                 },
                 changedelete = {
                     hl = 'GitSignsChange',
-                    text = '▎',
+                    text = '~',
                     numhl = 'GitSignsChangeNr',
                     linehl = 'GitSignsChangeLn',
                 },
