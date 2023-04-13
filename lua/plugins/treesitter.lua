ae77a4880c82799fa38343425719935e90054988
Jade Fox <guiltier.gear@gmail.com>
2023-04-13 02:37:17 -0700

    Reverted nvim-treesitter build option
    For some reason, using a function for the build option for
    nvim-treesitter doesn't seem to work properly, so I'm going back to just
    using the standard command version

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    build = function()
-        require('nvim-treesitter.install').update({ with_sync = true })
-    end,
+    build = ':TSUpdate',
     event = {
         'BufReadPre',
         'BufNewFile'
