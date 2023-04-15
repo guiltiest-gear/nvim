fd62b36799697923f7288f91b9e87413f35a67ef
Jade Fox <guiltier.gear@gmail.com>
2023-04-15 14:47:06 -0700

    Removed redundant config in which-key.lua

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,56 +7,10 @@ return {
         "`",
     },
     config = function()
-        local which_key = require('which-key')
+        local wk = require('which-key')
         local setup = {
-            plugins = {
-                marks = true,
-                registers = true,
-                spelling = {
-                    enabled = true,
-                    suggestions = 20,
-                },
-                presets = {
-                    operators = true,
-                    motions = true,
-                    text_objects = true,
-                    windows = true,
-                    nav = true,
-                    z = true,
-                    g = true,
-                },
-            },
-            operators = { gc = 'Comments' },
-            icons = {
-                breadcrumb = '»',
-                separator = '➜',
-                group = '+',
-            },
-            popup_mappings = {
-                scroll_down = '<c-d>',
-                scroll_up = '<c-u>',
-            },
-            window = {
-                border = 'rounded',
-                position = 'bottom',
-                margin = { 1, 0, 1, 0 },
-                padding = { 2, 2, 2, 2 },
-                winblend = 0,
-            },
-            layout = {
-                height = { min = 4, max = 25 },
-                width = { min = 20, max = 50 },
-                spacing = 3,
-                align = 'left',
-            },
             ignore_missing = true,
             hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' },
-            show_help = true,
-            triggers = 'auto',
-            triggers_blacklist = {
-                i = { 'j', 'k' },
-                v = { 'j', 'k' },
-            },
         }
 
         local mappings = {
@@ -138,7 +92,7 @@ return {
             nowait = true,
         }
 
-        which_key.setup(setup)
-        which_key.register(mappings, opts)
+        wk.setup(setup)
+        wk.register(mappings, opts)
     end,
 }
