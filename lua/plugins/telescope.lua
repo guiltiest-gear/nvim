4460e58f6773a4b567110e6e871883887b52667e
Jade Fox <guiltier.gear@gmail.com>
2023-04-15 13:22:14 -0700

    Added opts table to telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,35 +4,33 @@ return {
         'nvim-lua/plenary.nvim',
         {
             'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make'
-        }
+            build = 'make',
+        },
     },
     branch = '0.1.x',
     cmd = 'Telescope',
+    opts = {
+        defaults = {
+            path_display = { 'smart' },
+            file_ignore_patterns = { '.git/' },
+        },
+        pickers = {
+            find_files = {
+                find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+            },
+        },
+        extensions = {
+            fzf = {
+                theme = 'tokyonight',
+                fuzzy = true,
+                override_generic_sorter = true,
+                override_file_sorter = true,
+                case_mode = 'smart_case',
+            },
+        },
+    },
     config = function()
-        local telescope = require('telescope')
-        telescope.setup({
-            defaults = {
-                path_display = { 'smart' },
-                file_ignore_patterns = { '.git/' },
-            },
-            pickers = {
-                find_files = {
-                    find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
-                },
-            },
-            extensions = {
-                fzf = {
-                    theme = 'tokyonight',
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = 'smart_case',
-                },
-            },
-        })
-
         -- Load the extension
-        telescope.load_extension('fzf')
+        require('telescope').load_extension('fzf')
     end,
 }
