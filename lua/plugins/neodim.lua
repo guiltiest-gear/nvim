26a41fbd6d19a97ae20f37077fc124ca822af3b1
Jade Fox <guiltier.gear@gmail.com>
2023-04-15 12:57:49 -0700

    Changed neodim.lua config option to opts table

--- lua/plugins/neodim.lua
+++ lua/plugins/neodim.lua
@@ -2,20 +2,17 @@ return {
     'zbirenbaum/neodim',
     event = 'LspAttach',
     branch = 'v2',
-    config = function()
-        local neodim = require('neodim')
-        neodim.setup({
-            alpha = 0.75,
-            blend_color = '#000000',
-            update_in_insert = {
-                enable = true,
-                delay = 100,
-            },
-            hide = {
-                virtual_text = true,
-                signs = true,
-                underline = true,
-            },
-        })
-    end,
+    opts = {
+        alpha = 0.75,
+        blend_color = '#000000',
+        update_in_insert = {
+            enable = true,
+            delay = 100,
+        },
+        hide = {
+            virtual_text = true,
+            signs = true,
+            underline = true,
+        },
+    },
 }
