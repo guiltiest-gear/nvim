fef3c206cd8f4772d4dd4719c04e1c7db3b29491
Jade Fox <guiltier.gear@gmail.com>
2023-04-15 14:09:03 -0700

    Changed lualine.lua config option to opts table

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,36 +3,32 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     event = {
         'BufReadPre',
-        'BufNewFile'
+        'BufNewFile',
     },
-    config = function()
-        local lualine = require('lualine')
-        local navic = require('nvim-navic')
-        lualine.setup({
-            options = {
-                theme = 'tokyonight',
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                disabled_filetypes = { 'mason', 'dashboard', 'NvimTree' },
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = {
-                    'filename',
-                    {
-                        function()
-                            return navic.get_location()
-                        end,
-                        cond = function()
-                            return navic.is_available()
-                        end,
-                    },
+    opts = {
+        options = {
+            theme = 'tokyonight',
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+            disabled_filetypes = { 'mason', 'dashboard', 'NvimTree' },
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = {
+                'filename',
+                {
+                    function()
+                        return require('nvim-navic').get_location()
+                    end,
+                    cond = function()
+                        return require('nvim-navic').is_available()
+                    end,
                 },
-                lualine_x = { 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' },
             },
-        })
-    end,
+            lualine_x = { 'fileformat', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
+        },
+    },
 }
