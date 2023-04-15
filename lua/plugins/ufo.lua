b9646f957861a8b1b61d7dae5ff403fd76127ae2
Jade Fox <guiltier.gear@gmail.com>
2023-04-15 13:03:55 -0700

    Changed ufo.lua config option to opts table

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -3,14 +3,11 @@ return {
     dependencies = 'kevinhwang91/promise-async',
     event = {
         'BufReadPre',
-        'BufNewFile'
+        'BufNewFile',
     },
-    config = function()
-        local ufo = require('ufo')
-        ufo.setup({
-            provider_selector = function(bufnr, filetype, buftype)
-                return { 'treesitter', 'indent' }
-            end,
-        })
-    end,
+    opts = {
+        provider_selector = function(bufnr, filetype, buftype)
+            return { 'treesitter', 'indent' }
+        end,
+    },
 }
