51e7ab35ac0224ffa7e6b8d4f1ca0104877d7b10
Jade Fox <guiltier.gear@gmail.com>
2023-04-17 00:28:20 -0700

    Fixed find text keymap finally

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -38,7 +38,7 @@ map("n", "]b", ":BufferLineCycleNext<CR>", opts)
 
 -- Telescope keybindings
 map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+map("n", "<leader>fw", ":Telescope live_grep<CR>", opts)
 map("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 map("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
 map("n", "<leader>fm", ":Telescope man_pages<CR>", opts)
