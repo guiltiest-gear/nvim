12420709c91e9e0dae02f73d2df3df65362f82f4
Jade Fox <guiltier.gear@gmail.com>
2023-04-17 01:00:28 -0700

    Replaced tabs with spaces in bufferline.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,13 +1,13 @@
 return {
-	'akinsho/bufferline.nvim',
-	dependencies = 'nvim-tree/nvim-web-devicons',
-	version = 'v3.*',
-	event = 'BufEnter',
-	opts = {
-		options = {
-			mode = 'buffers',
-			diagnostics = 'nvim_lsp',
-			diagnostics_indicator = function(count, level, diagnostics_dict, context)
+    'akinsho/bufferline.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    version = 'v3.*',
+    event = 'BufEnter',
+    opts = {
+        options = {
+            mode = 'buffers',
+            diagnostics = 'nvim_lsp',
+            diagnostics_indicator = function(count, level, diagnostics_dict, context)
                 local s = ' '
                 for e, n in pairs(diagnostics_dict) do
                     local sym = e == 'error' and ' '
@@ -15,7 +15,7 @@ return {
                     s = s .. n .. sym
                 end
                 return s
-			end,
-		},
-	},
+            end,
+        },
+    },
 }
