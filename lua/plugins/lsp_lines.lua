c18467f288ba738576de045351655904c1cb48bf
Jade Fox <guiltier.gear@gmail.com>
2023-04-16 20:11:03 -0700

    Fixed minor issue in lsp_lines.lua

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local lsp_lines = require('lsp_lines')
         lsp_lines.setup()
-        vim.keymap.set('', '<leader>l', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
+        vim.keymap.set('', '<leader>l', lsp_lines.toggle, { desc = 'Toggle lsp_lines' })
         vim.diagnostic.config({
             virtual_text = false
         })
