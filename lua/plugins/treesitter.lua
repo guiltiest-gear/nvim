ee9569b19130aba03c7847b4d15dbe1469c7f801
Jade Fox <guiltier.gear@gmail.com>
2023-04-21 02:04:30 -0700

    Finally just selected only the treesitter parsers I need

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,23 @@ return {
         'BufNewFile'
     },
     opts = {
-        ensure_installed = 'all',
+        ensure_installed = {
+            'lua',
+            'luadoc',
+            'luap',
+            'cpp',
+            'markdown',
+            'markdown_inline',
+            'git_config',
+            'git_rebase',
+            'gitcommit',
+            'gitignore',
+            'bash',
+            'comment',
+            'diff',
+            'vim',
+            'vimdoc'
+        },
         ignore_install = { '' },
         auto_install = true
     },
