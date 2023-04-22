ef0b3b9fd08f8ce1f371b4775dabca5393a96c10
Jade Fox <guiltier.gear@gmail.com>
2023-04-22 01:19:13 -0700

    Improved autopairs.lua options

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,18 +1,13 @@
 return {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = function()
+    opts = {
+        disable_filetype = { 'TelescopePrompt', 'text' }
+    },
+    config = function(_, opts)
         local npairs = require('nvim-autopairs')
-        npairs.setup({
-            check_ts = true,
-            disable_filetype = { 'TelescopePrompt', 'text' },
-        })
-
+        npairs.setup(opts)
         local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-        local cmp_status_ok, cmp = pcall(require, 'cmp')
-        if not cmp_status_ok then
-            return
-        end
-        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({}))
+        require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done({}))
     end,
 }
