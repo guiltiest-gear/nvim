2bcd77cd925c45c2dfe5ca84fb044e9102474b04
Jade Fox <guiltier.gear@gmail.com>
2023-04-30 20:11:32 -0700

    Fix issue with LuaSnip and friendly-snippets
    See https://github.com/rafamadriz/friendly-snippets/issues/262 for more
    information regarding this

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
     config = function()
         local cmp = require 'cmp'
         local luasnip = require 'luasnip'
-        require('luasnip/loaders/from_vscode').lazy_load()
+        require('luasnip.loaders.from_vscode').load()
 
         local check_backspace = function()
             local col = vim.fn.col '.' - 1
