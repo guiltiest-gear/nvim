c5eb5f0e20168ec40b2e41bd357d168da367d902
Jade Fox <guiltier.gear@gmail.com>
2023-04-30 19:40:33 -0700

    Fixed icons and formatting in cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,68 +11,78 @@ return {
         {
             'L3MON4D3/LuaSnip',
             build = 'make install_jsregexp',
-            version = 'v1.*'
+            version = 'v1.*',
         },
-        'rafamadriz/friendly-snippets'
+        'rafamadriz/friendly-snippets',
     },
     event = 'InsertEnter',
     config = function()
-        local cmp = require('cmp')
-        local luasnip = require('luasnip')
+        local cmp = require 'cmp'
+        local luasnip = require 'luasnip'
         require('luasnip/loaders/from_vscode').lazy_load()
 
         local check_backspace = function()
-            local col = vim.fn.col('.') - 1
-            return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+            local col = vim.fn.col '.' - 1
+            return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s'
         end
 
         local kind_icons = {
-            Text = '',
-            Method = '',
-            Function = '',
-            Constructor = '',
-            Field = '',
-            Variable = '',
-            Class = '',
-            Interface = '',
-            Module = '',
-            Property = '',
-            Unit = '',
-            Value = '',
-            Enum = '',
-            Keyword = '',
-            Snippet = '',
-            Color = '',
-            File = '',
-            Reference = '',
-            Folder = '',
-            EnumMember = '',
-            Constant = '',
-            Struct = '',
-            Event = '',
-            Operator = '',
-            TypeParameter = '',
+            Array = ' ',
+            Boolean = ' ',
+            Class = ' ',
+            Color = ' ',
+            Constant = ' ',
+            Constructor = ' ',
+            Copilot = ' ',
+            Enum = ' ',
+            EnumMember = ' ',
+            Event = ' ',
+            Field = ' ',
+            File = ' ',
+            Folder = ' ',
+            Function = ' ',
+            Interface = ' ',
+            Key = ' ',
+            Keyword = ' ',
+            Method = ' ',
+            Module = ' ',
+            Namespace = ' ',
+            Null = ' ',
+            Number = ' ',
+            Object = ' ',
+            Operator = ' ',
+            Package = ' ',
+            Property = ' ',
+            Reference = ' ',
+            Snippet = ' ',
+            String = ' ',
+            Struct = ' ',
+            Text = ' ',
+            TypeParameter = ' ',
+            Unit = ' ',
+            Value = ' ',
+            Variable = ' ',
         }
 
-        cmp.setup({
+        cmp.setup {
             snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
-            mapping = cmp.mapping.preset.insert({
+            mapping = cmp.mapping.preset.insert {
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
                 ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
                 ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                ['<C-e>'] = cmp.mapping({
+                ['<C-e>'] = cmp.mapping {
                     i = cmp.mapping.abort(),
                     c = cmp.mapping.close(),
-                }),
+                },
                 -- Accept currently selected item. If none selected, `select` first item.
                 -- Set `select` to `false` to only confirm explicitly selected items.
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm { select = true },
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
@@ -101,7 +111,7 @@ return {
                     'i',
                     's',
                 }),
-            }),
+            },
             formatting = {
                 fields = { 'kind', 'abbr', 'menu' },
                 format = function(entry, vim_item)
@@ -143,15 +153,15 @@ return {
                     return false
                 end
                 -- Disable completion in comments
-                local context = require('cmp.config.context')
+                local context = require 'cmp.config.context'
                 -- Keep command mode completion enabled when cursor is in a comment
                 if vim.api.nvim_get_mode().mode == 'c' then
                     return true
                 else
-                    return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+                    return not context.in_treesitter_capture 'comment' and not context.in_syntax_group 'Comment'
                 end
             end,
-        })
+        }
 
         -- cmp-cmdline setup
         cmp.setup.cmdline('/', {
