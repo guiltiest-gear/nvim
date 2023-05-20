e09cac626e7d9a5aba33763e364e7546dc6e726a
Jade Fox <guiltier.gear@gmail.com>
2023-05-19 23:20:56 -0700

    Sorted dependencies in cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,13 +1,13 @@
 return {
     'hrsh7th/nvim-cmp',
     dependencies = {
-        'hrsh7th/cmp-buffer',
         'FelipeLema/cmp-async-path',
-        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-calc',
+        'hrsh7th/cmp-cmdline',
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-calc',
+        'saadparwaiz1/cmp_luasnip',
         'rafamadriz/friendly-snippets',
         {
             'L3MON4D3/LuaSnip',
