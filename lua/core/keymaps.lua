bd555a7a980d065ad5ffa88416d04dd311e5f10c
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 15:37:10 -0700

    Changed colons in keymaps.lua to <cmd>

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Open dashboard-nvim
-map('n', '<leader>d', ':Dashboard<CR>', opts)
+map('n', '<leader>d', '<cmd>Dashboard<CR>', opts)
 
 -- Better split navigation
 map('n', '<C-h>', '<C-w>h', opts)
@@ -15,33 +15,33 @@ map('n', '<C-k>', '<C-w>k', opts)
 map('n', '<C-l>', '<C-w>l', opts)
 
 -- Quit neovim
-map('n', '<leader>Q', ':qa<CR>', opts)
-map('n', '<leader>q', ':q<CR>', opts)
+map('n', '<leader>Q', '<cmd>qa<CR>', opts)
+map('n', '<leader>q', '<cmd>q<CR>', opts)
 
 -- Quick write
-map('n', '<leader>w', ':w<CR>', opts)
+map('n', '<leader>w', '<cmd>w<CR>', opts)
 
 -- Toggleterm.nvim keymap
-map('n', [[<C-\>]], ':ToggleTerm<CR>', opts)
+map('n', [[<C-\>]], '<cmd>ToggleTerm<CR>', opts)
 
 -- Resize splits with arrow keys
-map('n', '<M-l>', ':vertical resize +1<CR>', opts)
-map('n', '<M-h>', ':vertical resize -1<CR>', opts)
-map('n', '<M-k>', ':resize +1<CR>', opts)
-map('n', '<M-j>', ':resize -1<CR>', opts)
+map('n', '<M-l>', '<cmd>vertical resize +1<CR>', opts)
+map('n', '<M-h>', '<cmd>vertical resize -1<CR>', opts)
+map('n', '<M-k>', '<cmd>resize +1<CR>', opts)
+map('n', '<M-j>', '<cmd>resize -1<CR>', opts)
 
 -- Lazy keymap
-map('n', '<leader>Ll', ':Lazy<CR>', opts)
-map('n', '<leader>Lu', ':Lazy update<CR>', opts)
-map('n', '<leader>Ls', ':Lazy sync<CR>', opts)
-map('n', '<leader>LL', ':Lazy log<CR>', opts)
-map('n', '<leader>Lc', ':Lazy clean<CR>', opts)
-map('n', '<leader>Lp', ':Lazy profile<CR>', opts)
+map('n', '<leader>Ll', '<cmd>Lazy<CR>', opts)
+map('n', '<leader>Lu', '<cmd>Lazy update<CR>', opts)
+map('n', '<leader>Ls', '<cmd>Lazy sync<CR>', opts)
+map('n', '<leader>LL', '<cmd>Lazy log<CR>', opts)
+map('n', '<leader>Lc', '<cmd>Lazy clean<CR>', opts)
+map('n', '<leader>Lp', '<cmd>Lazy profile<CR>', opts)
 
 -- Move lines
 map('n', '<A-j>', '<cmd>m .+1<cr>==', opts)
 map('n', '<A-k>', '<cmd>m .-2<cr>==', opts)
 map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', opts)
 map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', opts)
-map('v', '<A-j>', ":m '>+1<cr>gv=gv", opts)
-map('v', '<A-k>', ":m '<-2<cr>gv=gv", opts)
+map('v', '<A-j>', "<cmd>m '>+1<cr>gv=gv", opts)
+map('v', '<A-k>', "<cmd>m '<-2<cr>gv=gv", opts)
