4eb1d433034e14e76c17729e59f93e3ac708bcd2
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 01:04:27 -0700

    Changed how treesj keymaps work

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,9 +2,9 @@ return {
     'Wansmer/treesj',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     keys = {
-        '<leader>m',
-        '<leader>j',
-        '<leader>s',
+        { '<leader>m', function() return require('treesj').toggle() end, desc = 'Toggle node under cursor' },
+        { '<leader>j', function() return require('treesj').join() end, desc = 'Join node under cursor' },
+        { '<leader>s', function() return require('treesj').split() end, desc = 'Split node under cursor' },
     },
     opts = {
         max_join_length = 150,
