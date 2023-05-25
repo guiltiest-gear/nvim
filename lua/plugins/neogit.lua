7966ad851035d6a4c608a8bdce509e293d337aa7
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 01:48:19 -0700

    Use lua api function to open neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,7 +2,13 @@ return {
     'TimUntersberger/neogit',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
-        { '<leader>gg', '<cmd>Neogit<CR>', desc = 'Neogit' },
+        {
+            '<leader>gg',
+            function()
+                return require('neogit').open()
+            end,
+            desc = 'Neogit',
+        },
     },
     opts = {
         kind = 'replace',
