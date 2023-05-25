e348fb032e123db64f994f08ff83e45c403dcc42
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 02:21:07 -0700

    Moved bufdelete.nvim to seperate file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,27 +19,6 @@ return {
         config = true,
     },
 
-    -- bufdelete.nvim
-    {
-        'famiu/bufdelete.nvim',
-        keys = {
-            {
-                '<leader>bk',
-                function()
-                    return require('bufdelete').bufdelete(0, false)
-                end,
-                desc = 'Delete the current buffer',
-            },
-            {
-                '<leader>bK',
-                function()
-                    return require('bufdelete').bufdelete(0, true)
-                end,
-                desc = 'Delete the current buffer forcefully',
-            },
-        },
-    },
-
     -- hlargs.nvim
     {
         'm-demare/hlargs.nvim',
