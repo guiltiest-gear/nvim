9d216120d87311c492bc7f5870de1ef3713ddfb3
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 15:57:00 -0700

    Use lazy.nvim keys spec to add lsp_lines toggle keymap

--- lua/plugins/lsp-lines.lua
+++ lua/plugins/lsp-lines.lua
@@ -1,10 +1,18 @@
 return {
     'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
     event = 'LspAttach',
+    keys = {
+        {
+            '<leader>l',
+            function()
+                return require('lsp_lines').toggle()
+            end,
+            { desc = 'Toggle lsp_lines' },
+        },
+    },
     config = function()
         local lsp_lines = require('lsp_lines')
         lsp_lines.setup()
-        vim.keymap.set('', '<leader>l', lsp_lines.toggle, { desc = 'Toggle lsp_lines' })
         vim.diagnostic.config {
             virtual_text = false,
         }
