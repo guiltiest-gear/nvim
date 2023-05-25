0b2c68494d08a450970fea1a3840168d14ad8254
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 02:23:51 -0700

    Moved trouble.nvim to seperate file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,20 +67,4 @@ return {
         event = 'InsertEnter',
         config = true,
     },
-
-    -- trouble.nvim
-    {
-        'folke/trouble.nvim',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        event = 'LspAttach',
-        keys = {
-            { '<leader>xx', '<cmd>TroubleToggle<CR>' },
-            { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>' },
-            { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>' },
-            { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>' },
-            { '<leader>xl', '<cmd>TroubleToggle loclist<CR>' },
-            { '<leader>xr', '<cmd>TroubleToggle lsp_references<CR>' },
-        },
-        config = true,
-    },
 }
