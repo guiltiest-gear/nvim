c1ad94c2060c20e84307f9da6f2807185a994f8c
Jade Fox <guiltier.gear@gmail.com>
2023-05-25 01:40:53 -0700

    Changed how telescope keymaps work

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,20 @@ return {
         },
     },
     branch = '0.1.x',
-    cmd = 'Telescope',
+    keys = {
+        { '<leader>ff', '<cmd>Telescope find_files<CR>' },
+        { '<leader>fw', '<cmd>Telescope live_grep<CR>' },
+        { '<leader>fb', '<cmd>Telescope buffers<CR>' },
+        { '<leader>fh', '<cmd>Telescope help_tags<CR>' },
+        { '<leader>fm', '<cmd>Telescope man_pages<CR>' },
+        { '<leader>fr', '<cmd>Telescope oldfiles<CR>' },
+        { '<leader>fR', '<cmd>Telescope registers<CR>' },
+        { '<leader>fk', '<cmd>Telescope keys<CR>' },
+        { '<leader>fc', '<cmd>Telescope commands<CR>' },
+        { '<leader>go', ':Telescope git_status<CR>' },
+        { '<leader>gb', ':Telescope git_branches<CR>' },
+        { '<leader>gc', ':Telescope git_commits<CR>' },
+    },
     opts = {
         defaults = {
             theme = 'tokyonight',
