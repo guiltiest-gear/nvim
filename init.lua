bc08be4a015c91ae20c92475fa7ed82e843b5b13
Jade Fox <guiltier.gear@gmail.com>
2023-05-26 16:54:41 -0700

    Use stylua on init.lua
    This is just done for the sake of consistency given every other lua file
    had stylua applied to it.

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-require 'core.options'
-require 'core.keymaps'
-require 'core.autocommands'
-require 'core.bootstrap'
+require('core.options')
+require('core.keymaps')
+require('core.autocommands')
+require('core.bootstrap')
