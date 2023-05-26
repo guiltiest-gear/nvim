f9d6f85bad59fdfaadac6776412bbdba1009e057
Jade Fox <guiltier.gear@gmail.com>
2023-05-26 14:19:31 -0700

    Added NeogitConsole to disabled_filetypes in lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -16,6 +16,7 @@ return {
                 'NeogitStatus',
                 'NeogitCommitView',
                 'NeogitPopup',
+                'NeogitConsole',
             },
         },
         sections = {
