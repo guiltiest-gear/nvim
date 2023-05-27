447bb9b5c9ef999c15329876a1fc40b68ab593aa
Jade Fox <guiltier.gear@gmail.com>
2023-05-26 23:40:42 -0700

    Replaced nvim-lsp-notify with fidget.nvim
    fidget.nvim isn't near as finicky as nvim-lsp-notify and it's far less
    intrusive, so this should have been done earlier.

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,40 +1,15 @@
 return {
-    -- nvim-notify
-    {
-        'rcarriga/nvim-notify',
-        event = {
-            'BufReadPost',
-            'BufNewFile',
-        },
-        opts = {
-            background_colour = '#000000',
-        },
-        config = function(_, opts)
-            local notify = require('notify')
-            notify.setup(opts)
-            vim.notify = notify
-        end,
+    'rcarriga/nvim-notify',
+    event = {
+        'BufReadPost',
+        'BufNewFile',
     },
-
-    -- nvim-lsp-notify
-    {
-        'mrded/nvim-lsp-notify',
-        dependencies = 'rcarriga/nvim-notify',
-        event = 'LspAttach',
-        opts = {
-            icons = {
-                spinner = {
-                    '┤',
-                    '┘',
-                    '┴',
-                    '└',
-                    '├',
-                    '┌',
-                    '┬',
-                    '┐',
-                },
-                done = false,
-            },
-        },
+    opts = {
+        background_colour = '#000000',
     },
+    config = function(_, opts)
+        local notify = require('notify')
+        notify.setup(opts)
+        vim.notify = notify
+    end,
 }
