8475255252c44c28d6f32dc6630681f7074e6a4b
Jade Fox <guiltier.gear@gmail.com>
2023-05-27 02:46:46 -0700

    Use an init option for nvim-notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -4,12 +4,10 @@ return {
         'BufReadPost',
         'BufNewFile',
     },
+    init = function()
+        vim.notify = require('notify')
+    end,
     opts = {
         background_colour = '#000000',
     },
-    config = function(_, opts)
-        local notify = require('notify')
-        notify.setup(opts)
-        vim.notify = notify
-    end,
 }
