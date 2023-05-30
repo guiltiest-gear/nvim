f68d84b79ff15bccb005f43afba482fe1dec5da8
Jade Fox <guiltier.gear@gmail.com>
2023-05-29 18:24:53 -0700

    Added bufferline.nvim jump keymaps

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,6 +10,66 @@ return {
         { ']b', '<cmd>BufferLineCycleNext<CR>' },
         { '<Tab>', '<cmd>BufferLineCycleNext<CR>' },
         { '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>' },
+        {
+            '<leader>1',
+            function()
+                return require('bufferline').go_to(1, true)
+            end,
+        },
+        {
+            '<leader>2',
+            function()
+                return require('bufferline').go_to(2, true)
+            end,
+        },
+        {
+            '<leader>3',
+            function()
+                return require('bufferline').go_to(3, true)
+            end,
+        },
+        {
+            '<leader>4',
+            function()
+                return require('bufferline').go_to(4, true)
+            end,
+        },
+        {
+            '<leader>5',
+            function()
+                return require('bufferline').go_to(5, true)
+            end,
+        },
+        {
+            '<leader>6',
+            function()
+                return require('bufferline').go_to(6, true)
+            end,
+        },
+        {
+            '<leader>7',
+            function()
+                return require('bufferline').go_to(7, true)
+            end,
+        },
+        {
+            '<leader>8',
+            function()
+                return require('bufferline').go_to(8, true)
+            end,
+        },
+        {
+            '<leader>9',
+            function()
+                return require('bufferline').go_to(9, true)
+            end,
+        },
+        {
+            '<leader>$',
+            function()
+                return require('bufferline').go_to(-1, true)
+            end,
+        },
     },
     opts = {
         options = {
