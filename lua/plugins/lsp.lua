b094169ad32406b8a8a2b7e502a8dbb5e1cd0501
Jade Fox <guiltier.gear@gmail.com>
2023-06-01 21:40:41 -0700

    Merged lsp-lines.lua into lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -158,4 +158,25 @@ return {
             },
         },
     },
+
+    -- lsp_lines.nvim
+    {
+        'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+        event = 'LspAttach',
+        init = function()
+            vim.diagnostic.config {
+                virtual_text = false,
+            }
+        end,
+        keys = {
+            {
+                '<leader>l',
+                function()
+                    return require('lsp_lines').toggle()
+                end,
+                desc = 'Toggle lsp_lines',
+            },
+        },
+        config = true,
+    },
 }
