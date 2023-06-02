fd428df400e60454d8e57217020cc8ca18303056
Jade Fox <guiltier.gear@gmail.com>
2023-06-01 20:59:31 -0700

    Moved branch option in neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,11 @@
 return {
     'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v2.x',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
     },
+    branch = 'v2.x',
     init = function()
         vim.g.neo_tree_remove_legacy_commands = 1
     end,
