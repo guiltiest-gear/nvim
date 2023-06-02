94b82c391aad9c5724b45e9d90683d218df042ba
Jade Fox <guiltier.gear@gmail.com>
2023-06-01 21:24:46 -0700

    Revert "Added more keymaps to dial.nvim"
    This reverts commit 6ec727b748d684c9859b2fb36efe5ea0d4f1f76e.

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -7,6 +7,7 @@ return {
                 return require('dial.map').inc_normal()
             end,
             expr = true,
+            desc = 'Increment',
         },
         {
             '<C-x>',
@@ -14,52 +15,7 @@ return {
                 return require('dial.map').dec_normal()
             end,
             expr = true,
-        },
-        {
-            'g<C-a>',
-            function()
-                return require('dial.map').inc_gnormal()
-            end,
-            expr = true,
-        },
-        {
-            'g<C-x>',
-            function()
-                return require('dial.map').dec_gnormal()
-            end,
-            expr = true,
-        },
-        {
-            '<C-a>',
-            function()
-                return require('dial.map').inc_visual()
-            end,
-            'v',
-            expr = true,
-        },
-        {
-            '<C-x>',
-            function()
-                return require('dial.map').dec_visual()
-            end,
-            'v',
-            expr = true,
-        },
-        {
-            'g<C-a>',
-            function()
-                return require('dial.map').inc_gvisual()
-            end,
-            'v',
-            expr = true,
-        },
-        {
-            'g<C-x>',
-            function()
-                return require('dial.map').dec_gvisual()
-            end,
-            'v',
-            expr = true,
+            desc = 'Decrement',
         },
     },
     config = function()
