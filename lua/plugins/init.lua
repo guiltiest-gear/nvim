a4898221293df8931dcffefb71559f8f65a75e8b
Jade Fox <guiltier.gear@gmail.com>
2023-06-06 00:27:45 -0700

    Improved Comment.nvim lazy loading

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,9 +12,9 @@ return {
     -- Comment.nvim
     {
         'numToStr/Comment.nvim',
-        event = {
-            'BufReadPost',
-            'BufNewFile',
+        keys = {
+            { 'gc', mode = { 'n', 'v' }, desc = 'Comment toggle linewise' },
+            { 'gb', mode = { 'n', 'v' }, desc = 'Comment toggle blockwise' },
         },
         config = true,
     },
