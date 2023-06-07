5bd02e78ac174682ee937d669bed32a8694fbf77
Jade Fox <guiltier.gear@gmail.com>
2023-06-06 21:39:13 -0700

    Convert vim-matchup config option to init

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -1,11 +1,11 @@
 return {
     'andymass/vim-matchup',
     dependencies = 'nvim-treesitter/nvim-treesitter',
+    init = function()
+        vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
+    end,
     event = {
         'BufReadPost',
         'BufNewFile',
     },
-    config = function()
-        vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
-    end,
 }
