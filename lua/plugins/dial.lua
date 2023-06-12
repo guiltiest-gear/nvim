dee71564149a89f4964f67c3af6d439156fc22bd
Jade Fox <guiltier.gear@gmail.com>
2023-06-11 17:35:36 -0700

    Updated dial.nvim config
    Added augend.integer.alias.decimal_int,
    augend.constant.alias.alpha, and
    augend.constant.alias.Alpha

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -23,8 +23,11 @@ return {
         require('dial.config').augends:register_group {
             default = {
                 augend.integer.alias.decimal,
+                augend.integer.alias.decimal_int,
                 augend.integer.alias.hex,
                 augend.constant.alias.bool,
+                augend.constant.alias.alpha,
+                augend.constant.alias.Alpha,
             },
         }
     end,
