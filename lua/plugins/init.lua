a345d05e0a5dfde481e2ba6170b836f6119a2509
Jade Fox <guiltier.gear@gmail.com>
2023-06-12 00:15:54 -0700

    Merged several plugin files into plugins/init.lua
    I would consider the configuration for these simple enough to merge back
    into init.lua to reduce unneeded files.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,6 +22,36 @@ return {
         config = true,
     },
 
+    -- trouble.nvim
+    {
+        'folke/trouble.nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        keys = {
+            { '<leader>xx', '<cmd>TroubleToggle<CR>' },
+            { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>' },
+            { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>' },
+            { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>' },
+            { '<leader>xl', '<cmd>TroubleToggle loclist<CR>' },
+            { '<leader>xr', '<cmd>TroubleToggle lsp_references<CR>' },
+        },
+        config = true,
+    },
+
+    -- nvim-hlslens
+    {
+        'kevinhwang91/nvim-hlslens',
+        dependencies = 'romainl/vim-cool',
+        keys = {
+            { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+            { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+            { '*', [[*<Cmd>lua require('hlslens').start()<CR>]] },
+            { '#', [[#<Cmd>lua require('hlslens').start()<CR>]] },
+            { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]] },
+            { 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]] },
+        },
+        config = true,
+    },
+
     -- hlargs.nvim
     {
         'm-demare/hlargs.nvim',
@@ -31,6 +61,62 @@ return {
             'cpp',
             'lua',
             'vim',
+        },
+        config = true,
+    },
+
+    -- symbols-outline.nvim
+    {
+        'simrat39/symbols-outline.nvim',
+        keys = { { '<leader>o', '<cmd>SymbolsOutline<CR>', desc = 'Open symbols-outline' } },
+        config = true,
+    },
+
+    -- todo-comments.nvim
+    {
+        'folke/todo-comments.nvim',
+        dependencies = 'nvim-lua/plenary.nvim',
+        event = {
+            'BufReadPost',
+            'BufNewFile',
+        },
+        keys = {
+            {
+                ']t',
+                function()
+                    return require('todo-comments').jump_next()
+                end,
+                desc = 'Jump to next todo comment',
+            },
+            {
+                '[t',
+                function()
+                    return require('todo-comments').jump_prev()
+                end,
+                desc = 'Jump to previous todo comment',
+            },
+        },
+        config = true,
+    },
+
+    -- windows.nvim
+    {
+        'anuvyklack/windows.nvim',
+        dependencies = {
+            'anuvyklack/animation.nvim',
+            'anuvyklack/middleclass',
+        },
+        init = function()
+            vim.o.winwidth = 10
+            vim.o.winminwidth = 10
+            vim.o.equalalways = false
+        end,
+        event = 'WinNew',
+        keys = {
+            { '<C-w>z', '<cmd>WindowsMaximize<CR>' },
+            { '<C-w>_', '<cmd>WindowsMaximizeVertically<CR>' },
+            { '<C-w>|', '<cmd>WindowsMaximizeHorizontally<CR>' },
+            { '<C-w>=', '<cmd>WindowsEqualize<CR>' },
         },
         config = true,
     },
