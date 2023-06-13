8811fbaffa96898063b05b0a1d91b428342e15d7
Jade Fox <guiltier.gear@gmail.com>
2023-06-13 01:50:41 -0700

    Split symbols-outline.nvim keys table
    It just looks weird joined, so I just split it purely for the purpose of
    readabilty and so it doesn't look as weird.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -188,7 +188,9 @@ return {
     -- symbols-outline.nvim
     {
         'simrat39/symbols-outline.nvim',
-        keys = { { '<leader>o', '<cmd>SymbolsOutline<CR>', desc = 'Open symbols-outline' } },
+        keys = {
+            { '<leader>o', '<cmd>SymbolsOutline<CR>', desc = 'Open symbols-outline' },
+        },
         opts = {
             symbols = {
                 File = { icon = ' ' },
