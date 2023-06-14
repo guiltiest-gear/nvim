d7eb2250b7ccb598cefc8142ff2f0c596c050a05
Jade Fox <guiltier.gear@gmail.com>
2023-06-13 23:49:20 -0700

    Moved todo-comments.nvim to ui.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,33 +39,6 @@ return {
         config = true,
     },
 
-    -- todo-comments.nvim
-    {
-        'folke/todo-comments.nvim',
-        dependencies = 'nvim-lua/plenary.nvim',
-        event = {
-            'BufReadPost',
-            'BufNewFile',
-        },
-        keys = {
-            {
-                ']t',
-                function()
-                    return require('todo-comments').jump_next()
-                end,
-                desc = 'Jump to next todo comment',
-            },
-            {
-                '[t',
-                function()
-                    return require('todo-comments').jump_prev()
-                end,
-                desc = 'Jump to previous todo comment',
-            },
-        },
-        config = true,
-    },
-
     -- windows.nvim
     {
         'anuvyklack/windows.nvim',
