d36578d21d97ce892dbaa3005c321ef71ebf0566
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:32:09 -0700

    Merged matchup.lua into editor.lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -41,6 +41,19 @@ return {
         config = true,
     },
 
+    -- vim-matchup
+    {
+        'andymass/vim-matchup',
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+        init = function()
+            vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
+        end,
+        event = {
+            'BufReadPost',
+            'BufNewFile',
+        },
+    },
+
     -- vim-illuminate
     {
         'RRethy/vim-illuminate',
