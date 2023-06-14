1074def7552d7bf2fabda3ce87fae805043d268c
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:28:06 -0700

    Merged dial.lua into coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -41,4 +41,40 @@ return {
             check_ts = true,
         },
     },
+
+    -- dial.nvim
+    {
+        'monaqa/dial.nvim',
+        keys = {
+            {
+                '<C-a>',
+                function()
+                    return require('dial.map').inc_normal()
+                end,
+                expr = true,
+                desc = 'Increment',
+            },
+            {
+                '<C-x>',
+                function()
+                    return require('dial.map').dec_normal()
+                end,
+                expr = true,
+                desc = 'Decrement',
+            },
+        },
+        config = function()
+            local augend = require('dial.augend')
+            require('dial.config').augends:register_group {
+                default = {
+                    augend.integer.alias.decimal,
+                    augend.integer.alias.decimal_int,
+                    augend.integer.alias.hex,
+                    augend.constant.alias.bool,
+                    augend.constant.alias.alpha,
+                    augend.constant.alias.Alpha,
+                },
+            }
+        end,
+    },
 }
