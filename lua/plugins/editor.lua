ff790cd4e5cb52c5d7f17a573529414f2c6b2d5f
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:34:46 -0700

    Merged toggleterm.lua into editor.lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -429,6 +429,28 @@ return {
         },
     },
 
+    -- toggleterm.nvim
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        keys = [[<C-\>]],
+        opts = {
+            open_mapping = [[<C-\>]],
+            size = 20,
+            hide_numbers = true,
+            shell = vim.o.shell,
+            shade_terminals = true,
+            shading_factor = 2,
+            persist_size = true,
+            start_in_insert = true,
+            direction = 'float',
+            close_on_exit = true,
+            float_opts = {
+                border = 'curved',
+            },
+        },
+    },
+
     -- numb.nvim
     {
         'nacro90/numb.nvim',
