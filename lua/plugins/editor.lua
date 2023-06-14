2c375b6f727f24f9ecaffbcd01fbe3b3465ba894
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:11:21 -0700

    Merged zen-mode.lua into editor.lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -184,4 +184,27 @@ return {
         end,
         opts = { labeled_modes = 'nx' },
     },
+
+    -- zen-mode.nvim
+    {
+        'folke/zen-mode.nvim',
+        keys = {
+            {
+                '<leader>z',
+                function()
+                    return require('zen-mode').toggle()
+                end,
+                desc = 'Toggle zen mode',
+            },
+        },
+        opts = {
+            plugins = {
+                kitty = {
+                    enabled = true,
+                    font = '+4',
+                },
+                twilight = { enabled = false },
+            },
+        },
+    },
 }
