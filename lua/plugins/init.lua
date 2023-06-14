a2f7c3728c1ecd4d8ada408a292d2303392c6379
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:18:21 -0700

    Moved Comment.nvim to coding.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,4 @@
 return {
-    -- Comment.nvim
-    {
-        'numToStr/Comment.nvim',
-        keys = {
-            { 'gc', mode = 'n', desc = 'Toggle comments (normal mode)' },
-            { 'gc', mode = 'v', desc = 'Toggle comments (visual mode)' },
-            { 'gb', mode = 'n', desc = 'Toggle block comments (normal mode)' },
-            { 'gb', mode = 'v', desc = 'Toggle block comments (visual mode)' },
-        },
-        config = true,
-    },
-
     -- hlargs.nvim
     {
         'm-demare/hlargs.nvim',
