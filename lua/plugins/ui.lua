399b8a114da7fe7a3671189b0b080088e83d1d6b
Jade Fox <guiltier.gear@gmail.com>
2023-06-13 23:42:44 -0700

    Merged illuminate.lua into ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -122,4 +122,22 @@ return {
             end,
         },
     },
+
+    -- vim-illuminate
+    {
+        'RRethy/vim-illuminate',
+        event = {
+            'BufReadPost',
+            'BufNewFile',
+        },
+        opts = {
+            filetypes_denylist = {
+                'help',
+                'text',
+            },
+        },
+        config = function(_, opts)
+            require('illuminate').configure(opts)
+        end,
+    },
 }
