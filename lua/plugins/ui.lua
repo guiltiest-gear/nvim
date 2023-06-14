efb385604ed18726580a37595adc006be4718cb5
Jade Fox <guiltier.gear@gmail.com>
2023-06-13 23:58:24 -0700

    Merged lualine.lua into ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -210,6 +210,67 @@ return {
         config = true,
     },
 
+    -- lualine.nvim
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        event = {
+            'BufReadPost',
+            'BufNewFile',
+        },
+        opts = {
+            options = {
+                theme = 'tokyonight',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    'mason',
+                    'dashboard',
+                    'NeogitStatus',
+                    'NeogitCommitView',
+                    'NeogitPopup',
+                    'NeogitConsole',
+                },
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = {
+                    'branch',
+                    'diff',
+                    {
+                        'diagnostics',
+                        sources = { 'nvim_lsp', 'nvim_diagnostic' },
+                    },
+                },
+                lualine_c = {
+                    'filename',
+                    {
+                        function()
+                            return require('nvim-navic').get_location()
+                        end,
+                        cond = function()
+                            return require('nvim-navic').is_available()
+                        end,
+                    },
+                },
+                lualine_x = {
+                    'fileformat',
+                    'filetype',
+                },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
+            },
+            extensions = {
+                'man',
+                'toggleterm',
+                'neo-tree',
+                'symbols-outline',
+                'trouble',
+                'lazy',
+            },
+        },
+    },
+
     -- dashboard-nvim
     {
         'nvimdev/dashboard-nvim',
