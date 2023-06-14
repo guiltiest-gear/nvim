484a947e03da87cf913980f1ed8b325cbdf353a4
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:30:59 -0700

    Merged treesj.lua into coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -77,4 +77,36 @@ return {
             }
         end,
     },
+
+    -- treesj
+    {
+        'Wansmer/treesj',
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+        keys = {
+            {
+                '<leader>m',
+                function()
+                    return require('treesj').toggle()
+                end,
+                desc = 'Toggle node under cursor',
+            },
+            {
+                '<leader>j',
+                function()
+                    return require('treesj').join()
+                end,
+                desc = 'Join node under cursor',
+            },
+            {
+                '<leader>s',
+                function()
+                    return require('treesj').split()
+                end,
+                desc = 'Split node under cursor',
+            },
+        },
+        opts = {
+            max_join_length = 150,
+        },
+    },
 }
