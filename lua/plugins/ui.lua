f58ca8e4b63a8851a1271c696075a2df10f58f38
Jade Fox <guiltier.gear@gmail.com>
2023-06-13 23:37:14 -0700

    Merged dressing.lua into ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,4 +27,20 @@ return {
             vim.notify = notify
         end,
     },
+
+    -- dressing.nvim
+    {
+        'stevearc/dressing.nvim',
+        init = function()
+            vim.ui.select = function(...)
+                require('lazy').load { plugins = { 'dressing.nvim' } }
+                return vim.ui.select(...)
+            end
+            vim.ui.input = function(...)
+                require('lazy').load { plugins = { 'dressing.nvim' } }
+                return vim.ui.input(...)
+            end
+        end,
+        lazy = true,
+    },
 }
