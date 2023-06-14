3397a887407ccf9263fb5618c7f546ed2d30e4d8
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 00:02:28 -0700

    Moved todo-comments.nvim and trouble.nvim into editor.lua

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,43 @@
+return {
+    -- todo-comments.nvim
+    {
+        'folke/todo-comments.nvim',
+        dependencies = 'nvim-lua/plenary.nvim',
+        event = {
+            'BufReadPost',
+            'BufNewFile',
+        },
+        keys = {
+            {
+                ']t',
+                function()
+                    return require('todo-comments').jump_next()
+                end,
+                desc = 'Jump to next todo comment',
+            },
+            {
+                '[t',
+                function()
+                    return require('todo-comments').jump_prev()
+                end,
+                desc = 'Jump to previous todo comment',
+            },
+        },
+        config = true,
+    },
+
+    -- trouble.nvim
+    {
+        'folke/trouble.nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        keys = {
+            { '<leader>xx', '<cmd>TroubleToggle<CR>' },
+            { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>' },
+            { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>' },
+            { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>' },
+            { '<leader>xl', '<cmd>TroubleToggle loclist<CR>' },
+            { '<leader>xr', '<cmd>TroubleToggle lsp_references<CR>' },
+        },
+        config = true,
+    },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,21 +11,6 @@ return {
         config = true,
     },
 
-    -- trouble.nvim
-    {
-        'folke/trouble.nvim',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-        keys = {
-            { '<leader>xx', '<cmd>TroubleToggle<CR>' },
-            { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<CR>' },
-            { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<CR>' },
-            { '<leader>xq', '<cmd>TroubleToggle quickfix<CR>' },
-            { '<leader>xl', '<cmd>TroubleToggle loclist<CR>' },
-            { '<leader>xr', '<cmd>TroubleToggle lsp_references<CR>' },
-        },
-        config = true,
-    },
-
     -- hlargs.nvim
     {
         'm-demare/hlargs.nvim',
