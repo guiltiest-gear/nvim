0f780e3c66f57d3596135b1b65bf74018b3f1a82
Jade Fox <guiltier.gear@gmail.com>
2023-06-13 23:50:19 -0700

    Moved windows.nvim to ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -187,4 +187,26 @@ return {
         },
         config = true,
     },
+
+    -- windows.nvim
+    {
+        'anuvyklack/windows.nvim',
+        dependencies = {
+            'anuvyklack/animation.nvim',
+            'anuvyklack/middleclass',
+        },
+        init = function()
+            vim.o.winwidth = 10
+            vim.o.winminwidth = 10
+            vim.o.equalalways = false
+        end,
+        event = 'WinNew',
+        keys = {
+            { '<C-w>z', '<cmd>WindowsMaximize<CR>' },
+            { '<C-w>_', '<cmd>WindowsMaximizeVertically<CR>' },
+            { '<C-w>|', '<cmd>WindowsMaximizeHorizontally<CR>' },
+            { '<C-w>=', '<cmd>WindowsEqualize<CR>' },
+        },
+        config = true,
+    },
 }
