f16727355c93a29081d207ce13c45b034f398fb7
Jade Fox <guiltier.gear@gmail.com>
2023-06-14 22:34:35 -0700

    Revert "Use the normal commands for bufdelete.nvim"
    This reverts commit 5e320219f4b58f01b67c4b84f71a17697fa732b5.
    
    Newest commit fixes the issue, this should work using the lua api
    again.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -441,8 +441,20 @@ return {
     {
         'famiu/bufdelete.nvim',
         keys = {
-            { '<leader>bk', '<cmd>Bdelete<CR>', desc = 'Delete the current buffer' },
-            { '<leader>bK', '<cmd>Bdelete!<CR>', desc = 'Delete the current buffer forcefully' },
+            {
+                '<leader>bk',
+                function()
+                    return require('bufdelete').bufdelete(0, false)
+                end,
+                desc = 'Delete the current buffer',
+            },
+            {
+                '<leader>bK',
+                function()
+                    return require('bufdelete').bufdelete(0, true)
+                end,
+                desc = 'Delete the current buffer forcefully',
+            },
         },
     },
 
