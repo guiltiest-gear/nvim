af149a3c97bc18541539241b0c1a2355b663d429
Jade Fox <guiltier.gear@gmail.com>
2023-06-15 20:04:37 -0700

    Use a better keys option for better-escape.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -578,6 +578,9 @@ return {
                 'jk',
                 'kk',
             },
+            keys = function()
+                return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+            end,
         },
     },
 
