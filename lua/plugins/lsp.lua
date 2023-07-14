2141232743796b4162d59f19d9e630f3c739979e
Jade Fox <guiltier.gear@gmail.com>
2023-07-14 15:34:48 -0700

    Added autoformat on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -109,6 +109,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     opts = function()
       local nls = require('null-ls')
+      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
       return {
         sources = {
           nls.builtins.formatting.stylua,
@@ -118,6 +119,22 @@ return {
           nls.builtins.diagnostics.luacheck,
           nls.builtins.diagnostics.cpplint,
         },
+        on_attach = function(client, bufnr)
+          -- Autoformat on save if supported
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_clear_autocmds {
+              group = augroup,
+              buffer = bufnr,
+            }
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format { bufnr = bufnr }
+              end,
+            })
+          end
+        end,
       }
     end,
   },
