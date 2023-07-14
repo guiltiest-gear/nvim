9a5b91a5a22a8a2cdbc6f7e629119b374981e49f
Jade Fox <guiltier.gear@gmail.com>
2023-07-14 15:05:43 -0700

    Completely overhaul nvim-cmp config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -86,126 +86,72 @@ return {
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-nvim-lua',
             'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
-            { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp', version = 'v1.*' },
+            {
+                'L3MON4D3/LuaSnip',
+                build = 'make install_jsregexp',
+                version = 'v1.*',
+                dependencies = {
+                    'rafamadriz/friendly-snippets',
+                    config = function()
+                        require('luasnip.loaders.from_vscode').lazy_load()
+                    end,
+                },
+                keys = {
+                    {
+                        '<tab>',
+                        function()
+                            return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
+                        end,
+                        expr = true,
+                        silent = true,
+                        mode = 'i',
+                    },
+                    {
+                        '<tab>',
+                        function()
+                            require('luasnip').jump(1)
+                        end,
+                        mode = 's',
+                    },
+                    {
+                        '<S-tab>',
+                        function()
+                            require('luasnip').jump(-1)
+                        end,
+                        mode = { 'i', 's' },
+                    },
+                },
+                opts = { history = true },
+            },
         },
-        init = function()
-            vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-        end,
         event = 'InsertEnter',
-        -- TODO: Fucking fix atrocious nvim-cmp config
-        config = function()
+        opts = function()
+            vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
             local cmp = require('cmp')
-            local luasnip = require('luasnip')
-            require('luasnip.loaders.from_vscode').lazy_load()
-
-            local check_backspace = function()
-                local col = vim.fn.col('.') - 1
-                return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-            end
-
-            local kind_icons = {
-                Array = ' ',
-                Boolean = ' ',
-                Class = ' ',
-                Color = ' ',
-                Constant = ' ',
-                Constructor = ' ',
-                Copilot = ' ',
-                Enum = ' ',
-                EnumMember = ' ',
-                Event = ' ',
-                Field = ' ',
-                File = ' ',
-                Folder = ' ',
-                Function = ' ',
-                Interface = ' ',
-                Key = ' ',
-                Keyword = ' ',
-                Method = ' ',
-                Module = ' ',
-                Namespace = ' ',
-                Null = ' ',
-                Number = ' ',
-                Object = ' ',
-                Operator = ' ',
-                Package = ' ',
-                Property = ' ',
-                Reference = ' ',
-                Snippet = ' ',
-                String = ' ',
-                Struct = ' ',
-                Text = ' ',
-                TypeParameter = ' ',
-                Unit = ' ',
-                Value = ' ',
-                Variable = ' ',
-            }
-
-            cmp.setup {
+            local defaults = require('cmp.config.default')()
+            return {
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                },
                 snippet = {
                     expand = function(args)
-                        luasnip.lsp_expand(args.body) -- For `luasnip` users.
+                        require('luasnip').lsp_expand(args.body)
                     end,
                 },
                 mapping = cmp.mapping.preset.insert {
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
-                    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-                    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                    ['<C-e>'] = cmp.mapping {
-                        i = cmp.mapping.abort(),
-                        c = cmp.mapping.close(),
-                    },
-                    -- Accept currently selected item. If none selected, `select` first item.
-                    -- Set `select` to `false` to only confirm explicitly selected items.
+                    ['<Tab>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm { select = true },
-                    ['<Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.expandable() then
-                            luasnip.expand()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif check_backspace() then
-                            fallback()
-                        else
-                            fallback()
-                        end
-                    end, {
-                        'i',
-                        's',
-                    }),
-                    ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, {
-                        'i',
-                        's',
-                    }),
+                    ['<S-CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
                 },
-                formatting = {
-                    fields = { 'kind', 'abbr', 'menu' },
-                    format = function(entry, vim_item)
-                        vim_item.kind = kind_icons[vim_item.kind]
-                        vim_item.menu = ({
-                            nvim_lsp = '',
-                            nvim_lua = '',
-                            luasnip = '',
-                            buffer = '',
-                            path = '',
-                            emoji = '',
-                        })[entry.source.name]
-                        return vim_item
-                    end,
-                },
-                sources = {
+                sources = cmp.config.sources {
                     { name = 'nvim_lsp' },
                     { name = 'nvim_lua' },
                     { name = 'luasnip' },
@@ -213,55 +159,78 @@ return {
                     { name = 'async_path' },
                     { name = 'calc' },
                 },
-                confirm_opts = {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = false,
+                formatting = {
+                    format = function(_, item)
+                        local icons = {
+                            Array = ' ',
+                            Boolean = ' ',
+                            Class = ' ',
+                            Color = ' ',
+                            Constant = ' ',
+                            Constructor = ' ',
+                            Copilot = ' ',
+                            Enum = ' ',
+                            EnumMember = ' ',
+                            Event = ' ',
+                            Field = ' ',
+                            File = ' ',
+                            Folder = ' ',
+                            Function = ' ',
+                            Interface = ' ',
+                            Key = ' ',
+                            Keyword = ' ',
+                            Method = ' ',
+                            Module = ' ',
+                            Namespace = ' ',
+                            Null = ' ',
+                            Number = ' ',
+                            Object = ' ',
+                            Operator = ' ',
+                            Package = ' ',
+                            Property = ' ',
+                            Reference = ' ',
+                            Snippet = ' ',
+                            String = ' ',
+                            Struct = ' ',
+                            Text = ' ',
+                            TypeParameter = ' ',
+                            Unit = ' ',
+                            Value = ' ',
+                            Variable = ' ',
+                        }
+                        if icons[item.kind] then
+                            item.kind = icons[item.kind] .. item.kind
+                        end
+                        return item
+                    end,
                 },
+                experimental = {
+                    ghost_text = {
+                        hl_group = 'CmpGhostText',
+                    },
+                },
+                sorting = defaults.sorting,
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
                 },
-                -- NOTE: Keep sorting disabled for now, the behavior isn't super desirable
-                --[[ sorting = {
-                    comparators = {
-                        cmp.config.compare.offset,
-                        cmp.config.compare.exact,
-                        cmp.config.compare.recently_used,
-                        cmp.config.compare.kind,
-                        cmp.config.compare.sort_text,
-                        cmp.config.compare.length,
-                        cmp.config.compare.order,
-                    },
-                }, ]]
-                experimental = { ghost_text = true },
-                enabled = function()
-                    -- Disable nvim-cmp in a telescope prompt
-                    local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
-                    if buftype == 'prompt' then
-                        return false
-                    end
-                    -- Disable completion in comments
-                    local context = require('cmp.config.context')
-                    -- Keep command mode completion enabled when cursor is in a comment
-                    if vim.api.nvim_get_mode().mode == 'c' then
-                        return true
-                    else
-                        return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
-                    end
-                end,
             }
-
-            -- cmp-cmdline setup
+        end,
+        config = function(_, opts)
+            local cmp = require('cmp')
+            cmp.setup(opts)
             cmp.setup.cmdline('/', {
                 mapping = cmp.mapping.preset.cmdline(),
-                sources = { { name = 'buffer' } },
+                sources = {
+                    { name = 'buffer' },
+                },
             })
-
             cmp.setup.cmdline(':', {
                 mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({ { name = 'path' } }, {
-                    { name = 'cmdline', option = { ignore_cmds = { 'Man', '!' } } },
-                }),
+                sources = cmp.config.sources {
+                    { name = 'path' },
+                    { name = 'cmdline' },
+                },
             })
         end,
     },
