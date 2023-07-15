a7dcc784080308bc585abac65f85d9bd0d677d08
Jade Fox <guiltier.gear@gmail.com>
2023-07-15 00:17:04 -0700

    Fix dap keymaps and luasnip keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -291,14 +291,14 @@ return {
           {
             '<leader>du',
             function()
-              require('dapui').toggle()
+              return require('dapui').toggle()
             end,
             desc = 'Dap UI',
           },
           {
             '<leader>de',
             function()
-              require('dapui').eval()
+              return require('dapui').eval()
             end,
             desc = 'Eval',
             mode = { 'n', 'v' },
@@ -329,112 +329,112 @@ return {
       {
         '<leader>dB',
         function()
-          require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
+          return require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
         end,
         desc = 'Breakpoint Condition',
       },
       {
         '<leader>db',
         function()
-          require('dap').toggle_breakpoint()
+          return require('dap').toggle_breakpoint()
         end,
         desc = 'Toggle Breakpoint',
       },
       {
         '<leader>dc',
         function()
-          require('dap').continue()
+          return require('dap').continue()
         end,
         desc = 'Continue',
       },
       {
         '<leader>dC',
         function()
-          require('dap').run_to_cursor()
+          return require('dap').run_to_cursor()
         end,
         desc = 'Run to Cursor',
       },
       {
         '<leader>dg',
         function()
-          require('dap').goto_()
+          return require('dap').goto_()
         end,
         desc = 'Go to line (no execute)',
       },
       {
         '<leader>di',
         function()
-          require('dap').step_into()
+          return require('dap').step_into()
         end,
         desc = 'Step Into',
       },
       {
         '<leader>dj',
         function()
-          require('dap').down()
+          return require('dap').down()
         end,
         desc = 'Down',
       },
       {
         '<leader>dk',
         function()
-          require('dap').up()
+          return require('dap').up()
         end,
         desc = 'Up',
       },
       {
         '<leader>dl',
         function()
-          require('dap').run_last()
+          return require('dap').run_last()
         end,
         desc = 'Run Last',
       },
       {
         '<leader>do',
         function()
-          require('dap').step_out()
+          return require('dap').step_out()
         end,
         desc = 'Step Out',
       },
       {
         '<leader>dO',
         function()
-          require('dap').step_over()
+          return require('dap').step_over()
         end,
         desc = 'Step Over',
       },
       {
         '<leader>dp',
         function()
-          require('dap').pause()
+          return require('dap').pause()
         end,
         desc = 'Pause',
       },
       {
         '<leader>dr',
         function()
-          require('dap').repl.toggle()
+          return require('dap').repl.toggle()
         end,
         desc = 'Toggle REPL',
       },
       {
         '<leader>ds',
         function()
-          require('dap').session()
+          return require('dap').session()
         end,
         desc = 'Session',
       },
       {
         '<leader>dt',
         function()
-          require('dap').terminate()
+          return require('dap').terminate()
         end,
         desc = 'Terminate',
       },
       {
         '<leader>dw',
         function()
-          require('dap.ui.widgets').hover()
+          return require('dap.ui.widgets').hover()
         end,
         desc = 'Widgets',
       },
