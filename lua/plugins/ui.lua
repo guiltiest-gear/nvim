098ef8f3bddd474afc347c29414771fe987388bc
Jade Fox <guiltier.gear@gmail.com>
2023-07-17 23:38:18 -0700

    Fix nvim-ufo provider_selector option

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -84,24 +84,7 @@ return {
     },
     opts = {
       provider_selector = function(_, filetype, buftype)
-        local function handleFallbackException(bufnr, err, providerName)
-          if type(err) == 'string' and err:match('UfoFallbackException') then
-            return require('ufo').getFolds(bufnr, providerName)
-          else
-            return require('promise').reject(err)
-          end
-        end
-        return (filetype == '' or buftype == 'nofile') and 'indent'
-          or function(bufnr)
-            return require('ufo')
-              .getFolds(bufnr, 'lsp')
-              :catch(function(err)
-                return handleFallbackException(bufnr, err, 'treesitter')
-              end)
-              :catch(function(err)
-                return handleFallbackException(bufnr, err, 'indent')
-              end)
-          end
+        return (filetype == '' or buftype == 'nofile') and 'indent' or { 'treesitter', 'indent' }
       end,
     },
   },
