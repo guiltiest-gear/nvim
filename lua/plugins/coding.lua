431039c17b063f40e08ad91740247886e91a35e7
Jade Fox <guiltier.gear@gmail.com>
2023-07-18 22:59:16 -0700

    Fix Comment.nvim keys table

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,10 +3,8 @@ return {
   {
     'numToStr/Comment.nvim',
     keys = {
-      { 'gc', mode = 'n', desc = 'Toggle comments (normal mode)' },
-      { 'gc', mode = 'v', desc = 'Toggle comments (visual mode)' },
-      { 'gb', mode = 'n', desc = 'Toggle block comments (normal mode)' },
-      { 'gb', mode = 'v', desc = 'Toggle block comments (visual mode)' },
+      { 'gc', mode = { 'n', 'v' }, desc = 'Toggle comments' },
+      { 'gb', mode = { 'n', 'v' }, desc = 'Toggle block comments' },
     },
     config = true,
   },
