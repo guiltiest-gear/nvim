878d8bcf414c15465bccdbcd10bcc2ab19a01de1
Jade Fox <guiltier.gear@gmail.com>
2023-07-29 21:56:42 -0700

    Added undotree

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -428,6 +428,9 @@ return {
   -- highlight-undo.nvim
   { 'tzachar/highlight-undo.nvim', keys = { 'u', '<C-r>' }, config = true },
 
+  -- undotree
+  { 'mbbill/undotree', keys = { { '<leader>u', '<cmd>UndotreeToggle<CR>', desc = 'Open undo tree' } } },
+
   -- bufferline.nvim
   {
     'akinsho/bufferline.nvim',
