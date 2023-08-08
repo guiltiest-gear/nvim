32bfc52b5903a41f0b8ddc915a02454cd9b3a34f
Jade Fox <guiltier.gear@gmail.com>
2023-08-08 13:56:59 -0700

    Added note for mason-tools-installer.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -115,6 +115,9 @@ return {
   -- Related: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621
   -- NOTE: Look into alternatives
   -- Related: https://github.com/mhartington/formatter.nvim and https://github.com/mfussenegger/nvim-lint
+  -- NOTE: mason-tools-installer.nvim seems to work for installing the tools I need, but doesn't seem to work properly.
+  -- Keep looking into this, maybe it's a config error on my end?
+  -- Related: https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
   {
     'jose-elias-alvarez/null-ls.nvim',
     dependencies = { 'williamboman/mason.nvim', 'nvim-lua/plenary.nvim' },
