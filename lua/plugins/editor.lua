bef8a38ac125a91f761ecc56802bbb446b9cd277
Jade Fox <guiltier.gear@gmail.com>
2023-08-07 22:23:11 -0700

    Removed stray blank lines in editor.lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -49,16 +49,13 @@ return {
     },
     config = function(_, opts)
       require('illuminate').configure(opts)
-
       local function map(key, dir, buffer)
         vim.keymap.set('n', key, function()
           require('illuminate')['goto_' .. dir .. '_reference'](false)
         end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. ' Reference', buffer = buffer })
       end
-
       map(']]', 'next')
       map('[[', 'prev')
-
       -- Set it after loading ftplugins
       vim.api.nvim_create_autocmd('FileType', {
         callback = function()
