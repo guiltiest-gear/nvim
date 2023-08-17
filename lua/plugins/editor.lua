b8286cb6f8aafb7f721033ea835e3ec4342fb5c3
Jade Fox <guiltier.gear@gmail.com>
2023-08-16 17:26:49 -0700

    Disable telescope preview for binaries and show hidden files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -655,19 +655,56 @@ return {
         desc = 'Search and checkout git commits',
       },
     },
-    opts = {
-      defaults = {
-        theme = 'tokyonight',
-        path_display = { 'smart' },
-        file_ignore_patterns = { '.git/' },
-        layout_strategy = 'horizontal',
-        layout_config = { prompt_position = 'top' },
-        sorting_strategy = 'ascending',
-      },
-      pickers = {
-        find_files = { find_command = { 'rg', '--color=never', '--files', '--hidden', '--glob', '!**/.git/*' } },
-      },
-    },
+    opts = function()
+      -- Don't preview binaries
+      local previewers = require('telescope.previewers')
+      local Job = require('plenary.job')
+      local new_maker = function(filepath, bufnr, opts)
+        filepath = vim.fn.expand(filepath)
+        ---@diagnostic disable-next-line: missing-fields
+        Job:new({
+          command = 'file',
+          args = { '--mime-type', '-b', filepath },
+          on_exit = function(j)
+            local mime_type = vim.split(j:result()[1], '/')[1]
+            if mime_type == 'text' then
+              previewers.buffer_previewer_maker(filepath, bufnr, opts)
+            else
+              -- maybe we want to write something to the buffer here
+              vim.schedule(function()
+                vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'BINARY' })
+              end)
+            end
+          end,
+        }):sync()
+      end
+
+      -- File and text search in hidden files and directories
+      local telescopeConfig = require('telescope.config')
+
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, '--hidden')
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, '--glob')
+      table.insert(vimgrep_arguments, '!**/.git/*')
+
+      return {
+        defaults = {
+          vimgrep_arguments = vimgrep_arguments,
+          buffer_previewer_maker = new_maker,
+          theme = 'tokyonight',
+          path_display = { 'smart' },
+          file_ignore_patterns = { '.git/' },
+          layout_strategy = 'horizontal',
+          layout_config = { prompt_position = 'top' },
+          sorting_strategy = 'ascending',
+        },
+        pickers = { find_files = { find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' } } },
+      }
+    end,
   },
 
   -- nvim-gomove
