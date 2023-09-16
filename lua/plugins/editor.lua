10a5669ca6c3baac5bc55b0df64ee8fb6062d571
Jade Fox <guiltier.gear@gmail.com>
2023-09-16 14:57:10 -0700

    Fix twilight.nvim dependency spec

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -182,9 +182,11 @@ return {
   {
     'folke/zen-mode.nvim',
     dependencies = {
-      'folke/twilight.nvim',
-      keys = { { '<leader>t', '<cmd>Twilight<CR>', desc = 'Toggle twilight.nvim' } },
-      config = true,
+      {
+        'folke/twilight.nvim',
+        keys = { { '<leader>t', '<cmd>Twilight<CR>', desc = 'Toggle twilight.nvim' } },
+        config = true,
+      },
     },
     opts = { plugins = { kitty = { enabled = true, font = '+4' } } },
     keys = {
