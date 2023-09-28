748476754f6a5e53a92c0f27291f565d64040ec7
Jade Fox <guiltier.gear@gmail.com>
2023-09-27 23:16:55 -0700

    Remove one mapping from better-escape.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -557,7 +557,7 @@ return {
     'max397574/better-escape.nvim',
     event = 'InsertCharPre',
     opts = {
-      mapping = { 'jj', 'jk', 'kk' },
+      mapping = { 'jj', 'jk' },
       keys = function()
         return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
       end,
