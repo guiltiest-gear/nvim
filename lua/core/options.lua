62972a1789123094fc30cbf856a21cf63bb7c837
Jade Fox <guiltier.gear@gmail.com>
2023-10-19 13:35:43 -0700

    Correctly disable matchup_matchparen_offscreen

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -151,7 +151,7 @@ g.loaded_perl_provider = 0
 g.loaded_python3_provider = 0
 
 -- Set matchparen options for vim-matchup
-g.matchup_matchparen_offscreen = { method = 'status_manual ' }
+g.matchup_matchparen_offscreen = {}
 
 -- Work async for vim-matchup
 g.matchup_matchparen_deferred = 1
