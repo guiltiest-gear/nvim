32427edc2445f02f260b837e389a4185b7728a75
Jade Fox <guiltier.gear@gmail.com>
2023-10-22 14:12:02 -0700

    Move neo-tree.nvim h and l keymaps into tables

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -187,24 +187,28 @@ return {
       },
       window = {
         mappings = {
-          ['h'] = function(state)
-            local node = state.tree:get_node()
-            if node.type == 'directory' and node:is_expanded() then
-              require('neo-tree.sources.filesystem').toggle_directory(state, node)
-            else
-              require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
-            end
-          end,
-          ['l'] = function(state)
-            local node = state.tree:get_node()
-            if node.type == 'directory' then
-              if not node:is_expanded() then
+          ['h'] = {
+            function(state)
+              local node = state.tree:get_node()
+              if node.type == 'directory' and node:is_expanded() then
                 require('neo-tree.sources.filesystem').toggle_directory(state, node)
-              elseif node:has_children() then
-                require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+              else
+                require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
               end
-            end
-          end,
+            end,
+          },
+          ['l'] = {
+            function(state)
+              local node = state.tree:get_node()
+              if node.type == 'directory' then
+                if not node:is_expanded() then
+                  require('neo-tree.sources.filesystem').toggle_directory(state, node)
+                elseif node:has_children() then
+                  require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+                end
+              end
+            end,
+          },
         },
       },
     },
