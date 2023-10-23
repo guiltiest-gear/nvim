49a67cdcadd95051c123693b64ffec5f3db444e1
Jade Fox <guiltier.gear@gmail.com>
2023-10-23 01:14:08 -0700

    Fix behavior of n and N keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -35,9 +35,9 @@ map('v', '<', '<gv')
 map('v', '>', '>gv')
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-map('n', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
+map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next search result' })
 map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
 map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
-map('n', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Previous search result' })
+map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Previous search result' })
 map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Previous search result' })
 map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Previous search result' })
