5e01a62c85255eb76e2b00189cdc879a24c38627
Jade Fox <guiltier.gear@gmail.com>
2023-11-01 22:31:48 -0700

    Join tables in ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -83,20 +83,10 @@ return {
     dependencies = 'kevinhwang91/promise-async',
     event = { 'BufReadPost', 'BufNewFile' },
     keys = {
-      {
-        'zR',
-        function()
-          return require('ufo').openAllFolds()
-        end,
-        desc = 'Open all folds',
-      },
-      {
-        'zM',
-        function()
-          return require('ufo').closeAllFolds()
-        end,
-        desc = 'Close all folds',
-      },
+      -- stylua: ignore start
+      { 'zR', function() return require('ufo').openAllFolds() end, desc = 'Open all folds' },
+      { 'zM', function() return require('ufo').closeAllFolds() end, desc = 'Close all folds' },
+      -- stylua: ignore end
     },
     opts = {
       provider_selector = function(_, filetype, buftype)
