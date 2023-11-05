9d6a19371e98be3883ef52817185aa9ee80c69fb
Jade Fox <guiltier.gear@gmail.com>
2023-11-04 23:05:36 -0700

    Remove pointless local variable in undotree config function

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -367,9 +367,8 @@ return {
     'mbbill/undotree',
     keys = { { '<leader>u', '<cmd>UndotreeToggle<CR>', desc = 'Open undo tree' } },
     config = function()
-      local g = vim.g
-      g.undotree_WindowLayout = 2
-      g.undotree_ShortIndicators = 1
+      vim.g.undotree_WindowLayout = 2
+      vim.g.undotree_ShortIndicators = 1
     end,
   },
 
