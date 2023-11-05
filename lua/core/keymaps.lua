a2f44b363db8d685ad3bd975cd64a64584bd9052
Jade Fox <guiltier.gear@gmail.com>
2023-11-05 01:07:23 -0800

    Add <leader>bb keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -57,3 +57,6 @@ map('n', ']e', diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
 map('n', '[e', diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
 map('n', ']w', diagnostic_goto(true, 'WARN'), { desc = 'Next Warning' })
 map('n', '[w', diagnostic_goto(false, 'WARN'), { desc = 'Prev Warning' })
+
+-- Switch to other buffer
+map('n', '<leader>bb', '<cmd>e #<CR>', { desc = 'Switch to other buffer' })
