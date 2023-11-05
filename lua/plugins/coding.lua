036de10fc80811b4f2b33e590d861f9b8d5e33a3
Jade Fox <guiltier.gear@gmail.com>
2023-11-04 22:35:40 -0700

    Replace mini.pairs with nvim-autopairs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -74,6 +74,7 @@ return {
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'saadparwaiz1/cmp_luasnip',
       'rcarriga/cmp-dap',
+      'windwp/nvim-autopairs',
       {
         'rafamadriz/friendly-snippets',
         config = function()
@@ -198,6 +199,7 @@ return {
     end,
     config = function(_, opts)
       local cmp = require('cmp')
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
       cmp.setup(opts)
       ---@diagnostic disable-next-line: missing-fields
       cmp.setup.cmdline('/', {
@@ -211,6 +213,8 @@ return {
       })
       ---@diagnostic disable-next-line: missing-fields
       cmp.setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, { sources = { { name = 'dap' } } })
+      -- nvim-autopairs integration
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end,
   },
 }
