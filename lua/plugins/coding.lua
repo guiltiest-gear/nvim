8058181264c894808957c960e20e097b85828a85
Jade Fox <guiltier.gear@gmail.com>
2023-11-13 17:13:29 -0800

    Use default config for LuaSnip

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -84,12 +84,7 @@ return {
           require('luasnip.loaders.from_vscode').lazy_load()
         end,
       },
-      {
-        'L3MON4D3/LuaSnip',
-        build = 'make install_jsregexp',
-        version = 'v2.*',
-        opts = { history = true, delete_check_events = 'TextChanged' },
-      },
+      { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp', version = 'v2.*', config = true },
     },
     event = 'InsertEnter',
     opts = function()
