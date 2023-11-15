0e3979ab8b5020621f0b29c173a402b8f96d7501
Jade Fox <guiltier.gear@gmail.com>
2023-11-15 15:59:29 -0800

    Add pumheight option to options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -132,6 +132,10 @@ opt.autowrite = true
 -- Maximum number of undo changes
 opt.undolevels = 10000
 
+-- Only display 20 items in a completion menu
+-- NOTE: Mixed feelings about this, maybe disable later
+opt.pumheight = 20
+
 -- Disable lsp logging
 vim.lsp.set_log_level('OFF')
 
