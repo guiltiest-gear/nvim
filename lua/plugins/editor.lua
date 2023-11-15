d892a89cfc27200a195ed4217945fdc01b4c023b
Jade Fox <guiltier.gear@gmail.com>
2023-11-14 16:25:41 -0800

    Join persistence.nvim keys table

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -458,27 +458,11 @@ return {
     event = 'BufReadPre',
     opts = { options = vim.opt.sessionoptions:get() },
     keys = {
-      {
-        '<leader>qs',
-        function()
-          return require('persistence').load()
-        end,
-        desc = 'Restore the session for the current dir',
-      },
-      {
-        '<leader>ql',
-        function()
-          return require('persistence').load({ last = true })
-        end,
-        desc = 'Restore the last session',
-      },
-      {
-        '<leader>qd',
-        function()
-          return require('persistence').stop()
-        end,
-        desc = 'Stop persistence',
-      },
+      -- stylua: ignore start
+      { '<leader>qs', function() return require('persistence').load() end, desc = 'Restore the session for the current dir' },
+      { '<leader>ql', function() return require('persistence').load({ last = true }) end, desc = 'Restore the last session' },
+      { '<leader>qd', function() return require('persistence').stop() end, desc = 'Stop persistence' },
+      -- stylua: ignore end
     },
   },
 
