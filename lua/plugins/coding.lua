32fdaca08f231a6324073b6808047db9d7f17a64
Jade Fox <guiltier.gear@gmail.com>
2024-01-26 12:27:58 -0800

    Change structure of has_words_before nvim-cmp function

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -149,7 +149,7 @@ return {
       local luasnip = require('luasnip')
       local cmp = require('cmp')
       local defaults = require('cmp.config.default')()
-      local has_words_before = function()
+      local function has_words_before()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
       end
