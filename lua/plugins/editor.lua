b94c9d64b0f7da453ee247a32a282ba5411bb862
Jade Fox <guiltier.gear@gmail.com>
2024-02-04 15:31:32 -0800

    Remove mini.trailspace
    Since I have an autocommand which strips trailing whitespace on write,
    pointing out trailing whitespace is kind of redundant as a whole, since
    it's going to be deleted anyway

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -478,9 +478,6 @@ return {
     },
   },
 
-  -- mini.trailspace
-  { 'echasnovski/mini.trailspace', event = 'InsertEnter', config = true },
-
   -- BufOnly.nvim
   { 'numToStr/BufOnly.nvim', keys = { { '<leader>bo', '<cmd>BufOnly<CR>', desc = 'Delete all other buffers' } } },
 
