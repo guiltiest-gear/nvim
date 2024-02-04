299639daf3bb484a81e3ce16f888ef77c29a1e8d
Jade Fox <guiltier.gear@gmail.com>
2024-02-04 14:50:02 -0800

    Use delta for diff program with actions-preview.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -224,21 +224,25 @@ return {
   {
     'aznhe21/actions-preview.nvim',
     dependencies = 'nvim-telescope/telescope.nvim',
-    opts = {
-      telescope = {
-        sorting_strategy = 'ascending',
-        layout_strategy = 'vertical',
-        layout_config = {
-          width = 0.8,
-          height = 0.9,
-          prompt_position = 'top',
-          preview_cutoff = 20,
-          preview_height = function(_, _, max_lines)
-            return max_lines - 15
-          end,
+    opts = function()
+      local hl = require('actions-preview.highlight')
+      return {
+        telescope = {
+          sorting_strategy = 'ascending',
+          layout_strategy = 'vertical',
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = 'top',
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
         },
-      },
-    },
+        highlight_command = { hl.delta('delta --side-by-side') },
+      }
+    end,
     keys = {
       {
         'gf',
