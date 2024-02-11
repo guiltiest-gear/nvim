0efdc5ce75b5b87eb4f48659a4a1f2c1ae8e9da7
Jade Fox <guiltier.gear@gmail.com>
2024-02-10 20:28:56 -0800

    Remove jj mapping from better-escape.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -513,7 +513,7 @@ return {
     'max397574/better-escape.nvim',
     event = 'InsertCharPre',
     opts = {
-      mapping = { 'jj', 'jk' },
+      mapping = { 'jk' },
       keys = function()
         return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
       end,
