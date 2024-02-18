d9b3552f90f82e00e2773d1834a4f3dcbeacb758
Jade Fox <guiltier.gear@gmail.com>
2024-02-18 01:28:39 -0800

    Revert "Add telescope-dap.nvim"
    This reverts commit 0fba811d8ef502382f18a7d2bbe2292910e6f2b1.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -300,7 +300,6 @@ return {
         ['<leader>g'] = { name = '+git' },
         ['<leader>x'] = { name = '+diagnostics/quickfix' },
         ['<leader>d'] = { name = '+debugger' },
-        ['<leader>dt'] = { name = '+debugger/telescope' },
       },
     },
     config = function(_, opts)
