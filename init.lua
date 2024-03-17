af377dc910ae65f9f0eced745fbf1019167f1272
Jade Fox <guiltier.gear@gmail.com>
2024-03-16 17:11:08 -0700

    Remove safe_require function from init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,5 @@
--- Safely require files
-local function safe_require(module)
-  local success, loaded_module = pcall(require, module)
-  if success then
-    return loaded_module
-  end
-  vim.cmd.echo('Error loading ' .. module)
-end
-
-safe_require('core.options')
-safe_require('core.keymaps')
-safe_require('core.autocommands')
-safe_require('core.bootstrap')
+-- Load all the files
+require('core.options')
+require('core.keymaps')
+require('core.autocommands')
+require('core.bootstrap')
