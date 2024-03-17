12697c1f5a9bdb21124a8a048d50413a028e6d5b
Jade Fox <guiltier.gear@gmail.com>
2024-03-16 17:45:33 -0700

    Give safe_require another shot this time

--- init.lua
+++ init.lua
@@ -1,8 +1,16 @@
 -- Enable experimental loader
 vim.loader.enable()
 
--- Load all the files
-require('core.options')
-require('core.keymaps')
-require('core.autocommands')
-require('core.bootstrap')
+-- Use pcall to load the modules
+local function safe_require(module)
+  local success, loaded = pcall(require, module)
+  if success then
+    return loaded
+  end
+  vim.api.nvim_echo({ { 'Error loading' .. module } }, true, {})
+end
+
+safe_require('core.options')
+safe_require('core.keymaps')
+safe_require('core.autocommands')
+safe_require('core.bootstrap')
