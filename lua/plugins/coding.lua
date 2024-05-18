cd1fb5f2a6250a7f00e9318900f6462b45225214
Jade Fox <guiltier.gear@gmail.com>
2024-05-18 16:20:50 -0700

    Drop BufOnly.nvim and mini.ai plugin specs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -132,29 +132,6 @@ return {
     end,
   },
 
-  -- mini.ai
-  -- NOTE: I don't really use this all that much, maybe time to drop or disable?
-  {
-    'echasnovski/mini.ai',
-    enabled = false,
-    keys = { { 'a', mode = { 'x', 'o' } }, { 'i', mode = { 'x', 'o' } } },
-    opts = function()
-      local ai = require('mini.ai')
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-          o = ai.gen_spec.treesitter({
-            a = { '@block.outer', '@conditional.outer', '@loop.outer' },
-            i = { '@block.inner', '@conditional.inner', '@loop.inner' },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
-          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
-          t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
-        },
-      }
-    end,
-  },
-
   -- nvim-cmp
   {
     'hrsh7th/nvim-cmp',
