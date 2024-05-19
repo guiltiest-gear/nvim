50cce22f5ddb5992ef0e68f0141d6f16e3cacec3
Jade Fox <guiltier.gear@gmail.com>
2024-05-19 16:20:40 -0700

    Remove leap.nvim cursor hiding hack

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -238,24 +238,6 @@ return {
       leap.add_default_mappings(true)
       vim.keymap.del({ 'x', 'o' }, 'x')
       vim.keymap.del({ 'x', 'o' }, 'X')
-
-      -- HACK: Remove this after upgrading to neovim 0.10
-      -- Hide cursor while in using leap.nvim, restore it afterwards
-      -- Related: https://github.com/neovim/neovim/issues/20793
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'LeapEnter',
-        callback = function()
-          vim.cmd.hi('Cursor', 'blend=100')
-          vim.opt.guicursor:append({ 'a:Cursor/lCursor' })
-        end,
-      })
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'LeapLeave',
-        callback = function()
-          vim.cmd.hi('Cursor', 'blend=0')
-          vim.opt.guicursor:remove({ 'a:Cursor/lCursor' })
-        end,
-      })
     end,
   },
 
