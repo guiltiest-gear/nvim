4e4def3c4a54e2c3fb5b464250613118c64de557
Jade Fox <guiltier.gear@gmail.com>
2024-05-19 14:31:54 -0700

    Change lazy loading logic for windows.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -112,13 +112,14 @@ return {
   {
     'anuvyklack/windows.nvim',
     dependencies = { 'anuvyklack/animation.nvim', 'anuvyklack/middleclass' },
-    event = 'WinNew',
     config = true,
     keys = {
       { '<C-w>z', '<cmd>WindowsMaximize<CR>', desc = 'Max out current window' },
       { '<C-w>_', '<cmd>WindowsMaximizeVertically<CR>', desc = 'Max out window height' },
       { '<C-w>|', '<cmd>WindowsMaximizeHorizontally<CR>', desc = 'Max out window width' },
       { '<C-w>=', '<cmd>WindowsEqualize<CR>', desc = 'Equalize windows' },
+      { '<C-w>v' },
+      { '<C-w>s' },
     },
   },
 
