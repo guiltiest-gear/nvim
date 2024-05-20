e07d66bfb5b83ce3edbf7de0fad8b078a29ef1d4
Jade Fox <guiltier.gear@gmail.com>
2024-05-20 16:26:14 -0700

    Switch back to using tags with bufferline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -315,7 +315,7 @@ return {
   {
     'akinsho/bufferline.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    -- version = '*',
+    version = '*',
     event = 'UIEnter',
     keys = {
       { '<Tab>', '<cmd>BufferLineCycleNext<CR>', desc = 'Go to next buffer' },
