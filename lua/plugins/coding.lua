4e1557f58deab5a92ba67a16ad1673d8d88d028e
Jade Fox <guiltier.gear@gmail.com>
2024-11-01 16:38:53 -0700

    Add note to blink.cmp plugin spec

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -196,6 +196,7 @@ return {
     enabled = false,
     lazy = false,
     dependencies = "rafamadriz/friendly-snippets",
+    -- NOTE: Wait until next version is released, as the keymaps I want are there
     version = "v0.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
