01ba5c53104851dfdc3387102152994df4f7267d
Jade Fox <guiltier.gear@gmail.com>
2024-11-02 20:36:25 -0700

    Add blink.cmp config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -198,7 +198,53 @@ return {
     version = "v0.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
-    opts = {},
+    opts = {
+      keymap = {
+        ["<C-b>"] = { "scroll_documentation_up" },
+        ["<C-f>"] = { "scroll_documentation_down" },
+        ["<C-e>"] = { "hide" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          "select_next",
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          "select_prev",
+          "snippet_backward",
+          "fallback",
+        },
+        ["<C-Space>"] = {
+          "show",
+          "show_documentation",
+          "hide_documentation",
+        },
+      },
+      accept = {
+        auto_brackets = { enabled = true },
+      },
+      trigger = {
+        signature_help = { enabled = true },
+      },
+      windows = {
+        autocomplete = {
+          selection = "auto_insert",
+          draw = "reversed",
+        },
+        documentation = {
+          auto_show = true,
+        },
+      },
+      sources = {
+        completion = {
+          enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        },
+        providers = {
+          lsp = { fallback_for = { "lazydev" } },
+          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+        },
+      },
+    },
   },
 
   -- nvim-cmp
