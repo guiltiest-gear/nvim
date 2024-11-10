01b71376dc76f74910d82546191027fb7b2c0872
Jade Fox <guiltier.gear@gmail.com>
2024-11-09 18:36:15 -0800

    Map K to hover for nvim-dap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -623,11 +623,34 @@ return {
     },
     config = function(_, opts)
       local dap = require("dap")
+      local api = vim.api
+      local keymap_restore = {}
+
       for debugger, config in pairs(opts.adapters) do
         dap.adapters[debugger] = config
       end
       for ft, config in pairs(opts.configurations) do
         dap.configurations[ft] = config
+      end
+
+      dap.listeners.after["event_initialized"]["me"] = function()
+        for _, buf in pairs(api.nvim_list_bufs()) do
+          local keymaps = api.nvim_buf_get_keymap(buf, "n")
+          for _, keymap in pairs(keymaps) do
+            if keymap.lhs == "K" then
+              table.insert(keymap_restore, keymap)
+              api.nvim_buf_del_keymap(buf, "n", "K")
+            end
+          end
+        end
+        api.nvim_set_keymap("n", "K", '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
+      end
+
+      dap.listeners.after["event_terminated"]["me"] = function()
+        for _, keymap in pairs(keymap_restore) do
+          api.nvim_buf_set_keymap(keymap.buffer, keymap.mode, keymap.lhs, keymap.rhs, { silent = keymap.silent == 1 })
+        end
+        keymap_restore = {}
       end
 
       vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
