e00e23c8fa0384694aa14f7ce20d7fad265d494e
Jade Fox <guiltier.gear@gmail.com>
2024-11-10 00:23:47 -0800

    Add nvim-dap config for c files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -616,6 +616,18 @@ return {
             stopOnEntry = false,
           },
         },
+        c = {
+          {
+            name = "Launch file",
+            type = "codelldb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+          },
+        },
         javascript = {
           {
             name = "Debug with Firefox",
