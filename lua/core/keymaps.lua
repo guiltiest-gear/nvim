2416bb52374d0f2ec8ddfe41a81511de24c17f03
Jade Fox <guiltier.gear@gmail.com>
2025-04-19 15:41:06 -0700

    Remove diagnostic goto keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -68,25 +68,6 @@ map(
   { expr = true, desc = "Previous search result" }
 )
 
--- Jump to diagnostics
----@param next boolean
----@param severity? string|integer
----@return function
-local function diagnostic_goto(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity })
-  end
-end
-
-map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
 -- Switch to other buffer
 map("n", "<leader>bb", "<cmd>e #<CR>", { desc = "Switch to other buffer" })
 
