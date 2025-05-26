43e8a1db73d5f306f8d5366ef1b8799ee00fdc69
Jade Fox <guiltier.gear@gmail.com>
2025-05-26 01:24:18 -0700

    Load vim-cool on VeryLazy
    The mess with the key table was stupid, just use VeryLazy

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -66,10 +66,7 @@ return {
   },
 
   -- vim-cool
-  { "romainl/vim-cool", keys = { "/", "?", "*", "#", "g*", "g#", "n", "N" } },
-  -- HACK: There doesn't seem to be an autocommand event to detect when you start
-  -- searching, so this will have to do until I can find an event for that or until neovim creates that event
-  -- Related: https://github.com/neovim/neovim/issues/18879
+  { "romainl/vim-cool", event = "VeryLazy" },
 
   -- neo-tree.nvim
   {
