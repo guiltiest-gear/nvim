79d5e7c1c95aecf5676fde7380eee184cf146429
Jade Fox <guiltier.gear@gmail.com>
2025-07-16 15:47:50 -0700

    Remove flit.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -263,20 +263,6 @@ return {
     end,
   },
 
-  -- flit.nvim
-  {
-    "ggandor/flit.nvim",
-    dependencies = "ggandor/leap.nvim",
-    opts = { labeled_modes = "nx" },
-    keys = function()
-      local ret = {}
-      for _, key in ipairs({ "f", "F", "t", "T" }) do
-        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-      end
-      return ret
-    end,
-  },
-
   -- neogit
   {
     "NeogitOrg/neogit",
