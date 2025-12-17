af1b2651c533e4e42f8fd5ea5f868bf9ef0bd0b9
Jade Fox <guiltier.gear@gmail.com>
2025-12-17 14:00:45 -0800

    Fix preview_filter match patterns

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -252,7 +252,7 @@ return {
       preview_filter = function(ch0, ch1, ch2)
         return not (
           ch1:match("%s")
-          or ch0:match("%w") and ch1:match("%w") and ch2:match("%w")
+          or ch0:match("%a") and ch1:match("%a") and ch2:match("%a")
         )
       end,
     },
