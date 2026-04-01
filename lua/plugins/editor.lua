f7056d37eb6b737e56106ebd4a624bfd980adb93
Jade Fox <guiltier.gear@gmail.com>
2026-04-01 12:19:47 -0700

    Add parentheses around leap.nvim preview_filter conditions

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -265,7 +265,7 @@ return {
       preview_filter = function(ch0, ch1, ch2)
         return not (
           ch1:match("%s")
-          or ch0:match("%a") and ch1:match("%a") and ch2:match("%a")
+          or (ch0:match("%a") and ch1:match("%a") and ch2:match("%a"))
         )
       end,
     },
